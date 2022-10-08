088fe635488dd783995db3526a0c00e3f80ed717
sniochup <snioszekpawel@gmail.com>
2022-10-08 17:10:21 +0200

    WIP flask api

--- server.py
+++ server.py
@@ -2,32 +2,104 @@ from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
-f = open('diseases.json')
-db = json.load(f)
 
-@app.route('/doctor', methods=['GET'])
+
+@app.route('/doctor', methods=['POST'])
 def get_drugs():
     data = request.get_json()
     diseases = data["diseases"]
+    exclude_list = data["excludeList"]
 
-    if "excludeList" in data
-
-    return jsonify()
-
-
-    {
-  "diseases": [
-    "diseasesName",
-    "diseasesName"
-  ],
-  "excludeList": [
-    "excludeSubstances1",
-    "excludeSubstances2"
-  ]
-}
+    return jsonify({
+        "recommendedDrugs": [
+            {
+                "drugName": "drugName1",
+                "substanceName": "substanceName1",
+                "diseases": [
+                    "diseasesName1",
+                    "diseasesName2"
+                ],
+                "activeCompounds": [
+                    "compounds1",
+                    "compounds2"
+                ]
+            },
+            {
+                "drugName": "drugName2",
+                "substanceName": "substanceName2",
+                "diseases": [
+                    "diseasesName1",
+                    "diseasesName2"
+                ],
+                "activeCompounds": [
+                    "compounds1",
+                    "compounds2"
+                ]
+            }
+        ],
+        "otherRecommendations": [
+            [
+                {
+                    "drugName": "drugName1",
+                    "substanceName": "substanceName1",
+                    "diseases": [
+                        "diseasesName1",
+                        "diseasesName2"
+                    ],
+                    "activeCompounds": [
+                        "compounds1",
+                        "compounds2"
+                    ]
+                },
+                {
+                    "drugName": "drugName2",
+                    "substanceName": "substanceName2",
+                    "diseases": [
+                        "diseasesName1",
+                        "diseasesName2"
+                    ],
+                    "activeCompounds": [
+                        "compounds1",
+                        "compounds2"
+                    ]
+                }
+            ],
+            [
+                {
+                    "drugName": "drugName1",
+                    "substanceName": "substanceName1",
+                    "diseases": [
+                        "diseasesName1",
+                        "diseasesName2"
+                    ],
+                    "activeCompounds": [
+                        "compounds1",
+                        "compounds2"
+                    ]
+                },
+                {
+                    "drugName": "drugName2",
+                    "substanceName": "substanceName2",
+                    "diseases": [
+                        "diseasesName1",
+                        "diseasesName2"
+                    ],
+                    "activeCompounds": [
+                        "compounds1",
+                        "compounds2"
+                    ]
+                }
+            ]
+        ]
+    }
+    )
 
 
 @app.route('/patient', methods=['GET'])
 def add_income():
     data = request.get_json()
     return jsonify("dupa")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
